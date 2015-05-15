e4ad0955b56fd2f5fa26acaaa106bc24da63aa0e
Michael Dmytriaha <mdmytiaha@playtika.com.ua>
2015-05-15 11:30:18 +0300

    N2T: week4: issignment complete

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -7,5 +7,58 @@
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
-
-// Put your code here.
+(START)
+    @SCREEN
+    D=A
+    @addr
+    M=D     // address = 16384 screen's base address
+    
+    @8192
+    D=A
+    @n
+    M=D     // n = 8192
+    @i
+    M=1     // i = 1
+    @KBD
+    D=M
+    @SET1
+    D;JGT
+    @SET0
+    D;JEQ
+    
+(SET0)
+    @R0
+    M=0
+    @LOOP
+    0;JMP
+(SET1)
+    @R0
+    M=-1
+    @LOOP
+    0;JMP
+(LOOP)
+    @i
+    D=M
+    @n
+    D=D-M
+    @END
+    D;JGT   // if i>n goto END
+    
+    @R0
+    D=M
+    @addr
+    A=M
+    M=D    // RAM[addr] = RAM[0]
+    
+    @i
+    M=M+1
+    @1
+    D=A
+    @addr
+    M=D+M   // addr = addr + 1
+    @LOOP
+    0;JMP
+    
+(END)   
+    @START
+    0;JMP
--- nand2tetris/projects/04/mult/Mult.asm
+++ nand2tetris/projects/04/mult/Mult.asm
@@ -6,4 +6,26 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+    @i      // i refers to some mem. location.
+    M=1     // i=1
+    @R2    // R2 refers to some mem. location.
+    M=0     // R2=0
+
+(LOOP)
+    @i
+    D=M     // D=i
+    @R1
+    D=D-M   // D=i-R1
+    @END
+    D;JGT   // If (i-R1)>0 goto END
+    @R0
+    D=M     // D=R0
+    @R2
+    M=D+M   // R2=R0+R2
+    @i
+    M=M+1   // i=i+1
+    @LOOP
+    0;JMP   // Goto LOOP
+(END)
+    @END
+    0;JMP   // Infinite loop
